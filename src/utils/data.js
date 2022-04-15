852804c927d41c7687dfea2858f88c713b442f7d
kienhuynh279 <kienhuynh279@gmail.com>
2022-04-15 19:33:32 +0700

    Row Container

--- src/utils/data.js
+++ src/utils/data.js
@@ -6,7 +6,7 @@ import C3 from '../img/c3.png'
 export const heroData = [
     {id: 1, name: 'Icecream', desc: 'Chocolate & Vanilla', price: '5.25', imgSrc: I1},
     {id: 2, name: 'Strawberries', desc: 'Fresh Strawberries', price: '10.25', imgSrc: F1},
-    {id: 3, name: 'Chicken Kebab', desc: 'Mixed Kebab Plate', price: '8.25', imgSrc: C3},
+    {id: 3, name: 'Chicken Kebab', desc: 'Mixed Kebab P late', price: '8.25', imgSrc: C3},
     {id: 4, name: 'Fish Kebab', desc: 'Mixed Fish Kebab', price: '5.25', imgSrc: Fi1}
 ]
 
